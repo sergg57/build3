c55996a5ecdffa467283daf76a7fd277c9a59c13
Сергей Гришин <serg@iMac-Sergej.local>
2017-06-05 18:17:38 +0300

    полностью закончил вёрстку 4-х страниц

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,6 +1,4 @@
 
-/* ---------- BLUR for form om page_works------------------------------- */
-
 $(document).ready(function(){
   blur();
 })
@@ -14,33 +12,13 @@ function blur() {
       blur = $('.feedback-form__blur'),
       posY = blurSection.offset().top - blur.offset().top, //   текущее положение элемента относительно документа.
       posY = posY - blur.height()/2,
-      //blurHeight = blur.height()/2,
       posX = blurSection.offset().left - blur.offset().left;
-
-      /*
-      console.log('imgWidth=', imgWidth);
-      console.log('blurSection=', blurSection);
-      console.log('blur=', blur);
-      console.log('posY=', posY);
-      console.log('posX=', posX);
-      console.log('blur.offset().bottom=', blur.height());
-      console.log('blurHeight=', blurHeight);
-
-
-      console.log('blurSection.offset().top =',blurSection.offset().top ,'-',' blur-posY=', blur.offset().top);
-      console.log('blurSection.offset().left=',blurSection.offset().left, '-','blur-posX=', blur.offset().left);
-      */
 
   blur.css({
       'background-size': imgWidth + 'px' + ' ' + 'auto',
       'background-position': (posX) + 'px' + ' ' + (posY-50)  +'px'
 
   });
-  /*
-    console.log('blur.css', blur.css('background-size'));
-    console.log('blur.css', blur.css('background-position'));
-    console.log('blurCSS=', blur);
- */
 }
 
 
@@ -129,7 +107,93 @@ $(function () {
     slider.downInit();
 
 });
+
 /* -------------- END slider ------------------------------------ */
+/*------ Blog-active ----------------------------------*/
+
+var menu = (function () {
+
+    var menuItem = $('.item__link'),
+        menuList = $('.blog__list'),
+        menuFlag = $('.blog__flag'),
+        leftColumn = $('.blog__content-left'),
+        rightColumn = $('.blog__content-right');
+        //menuBlogItem = $('.blog__item');
+
+    console.log('menuItem=', menuItem);
+    console.log('menuList=', menuList);
+    //console.log('menuBlogItem=',menuBlogItem);
+    console.log('leftColumn-1=', leftColumn);
+    console.log('rightColumn-1=', rightColumn);
+
+    return {
+
+        setActive:function () {
+            menuItem.on('click', function (e) {
+                menuItem.removeClass('active');
+                $(this).addClass('active');
+                console.log('menuItem-3=', this);
+            });
+
+        },
+        fixed: function () {
+            $(window).scroll(function()  {
+
+                if ($(window).scrollTop() > 400) {
+                    menuList.addClass('blog__list_fixed');
+                    menuFlag.addClass('blog__flag_fixed');
+                    //console.log('posScroll=', $(window).scrollTop());
+                    //console.log('menuList-2=', menuList);
+                } else {
+                    menuList.removeClass('blog__list_fixed');
+                    menuFlag.removeClass('blog__flag_fixed');
+                    //console.log('posScroll=', $(window).scrollTop());
+                    //console.log('menuList-3=', menuList);
+                }
+            });
+        },
+        flag: function () {
+            menuFlag.on('click', function (e) {
+                e.preventDefault();
+                console.log('leftColumn-2=', leftColumn);
+
+                if (leftColumn.css('display') == 'block') {
+                    leftColumn.hide();
+                    //rightColumn.removeClass('blog__content-right_active-menu');
+                    rightColumn.css('width', '100%');
+                    //menuBlogItem.css('padding-right', '15px');
+                    console.log('leftColumn-2=', leftColumn.css('display'));
+                    console.log('rightColumn-2=', rightColumn.css('width'));
+                    //console.log('menuBlogItem-2-1=', menuBlogItem.css('padding-right'));
+                } else {
+                    leftColumn.show();
+                    //rightColumn.addClass('blog__content-right_active-menu');
+                    rightColumn.css('width', '75%');
+                    console.log('leftColumn-3=', leftColumn.css('display'));
+                    console.log('rightColumn-3=', rightColumn.css('width'));
+                    //console.log('menuBlogItem-3-1=', menuBlogItem.css('padding-right'));
+                }
+            });
+        }
+
+
+    };
+
+
+})();
+
+ $(function () {
+     console.log('menuItem-1=', 1);
+     menu.setActive();
+     menu.fixed();
+     menu.flag();
+     //console.log('menuItem-2=',menu.setActive());
+
+ });
+
+/* ---------- BLUR for form page_works------------------------------- */
+
+
 
 
 /* Модуль для прелоадера - preloader  */
@@ -209,11 +273,15 @@ $(document).ready(function(){
 /* ---------------------- END humburger ---------------------------- */
 
 
-/* Модуль для параллакса по скроллу на странице about для секции - hero  */
+/* Модуль для параллакса по скроллу на страницах about, works, blog - для секции - hero  */
 var paralax = (function () {
     var bg = document.querySelector('.hero__bg'); // для картинки в заголовке
     var user=document.querySelector('.user.hero__user-block'); // для блока - пользователя - user-block
     var sectionText =document.querySelector('.section-title'); // для Portfolio
+
+    var bg_blog = document.querySelector('.hero-blog__bg'); // для картинки в заголовке on page-blog
+    var user_blog=document.querySelector('.user.hero-blog__user-block'); // для блока - пользователя - user-block
+
 
     return {
         move: function (block, windowScroll, strafeAmount) {
@@ -228,9 +296,16 @@ var paralax = (function () {
 
         },
         init: function (wScroll) {
-            this.move(bg, wScroll, 45);
-            this.move(sectionText,wScroll,30);
-            this.move(user,wScroll, 3);
+            if (bg != null) {
+                this.move(bg, wScroll, 45);
+                this.move(sectionText, wScroll, 30);
+                this.move(user, wScroll, 3);
+            }
+            if (bg_blog != null) {
+                this.move(bg_blog, wScroll, 45);
+                this.move(user_blog, wScroll, 3);
+            }
+
         }
     };
 }());
@@ -279,6 +354,65 @@ var parallaxmouse = (function () {
 window.addEventListener('mousemove', parallaxmouse.moveLayers);
 
 /* ---------------------- END parallaxmouse ---------------------------- */
+
+
+
+/*------ Blog-active ----------------------------------*/
+/*
+var menu = (function() {
+
+    var leftColumn = $('.blog__content_left'),
+        rightColumn = $('.blog__content_right'),
+        menuList = $('.blog__content_list'),
+        menuItem = $('.blog__content_list-item'),
+        menuIcon = $('.blog__content_list-icon');
+
+    return {
+
+        setActive: function () {
+            menuItem.on('click', function (e) {
+                menuItem.removeClass('active-link');
+                $(this).addClass('active-link');
+            });
+        },
+
+        fixed: function () {
+            $(window).scroll(function()  {
+
+                if ($(window).scrollTop() > 900) {
+                    menuList.addClass('blog__content_list_fixed');
+                } else {
+                    menuList.removeClass('blog__content_list_fixed');
+                }
+            });
+        },
+
+        flag: function () {
+            menuIcon.on('click', function (e) {
+                e.preventDefault();
+
+                if (leftColumn.css('display') == 'block') {
+                    leftColumn.hide();
+                    rightColumn.removeClass('blog__content_right_active-menu');
+                } else {
+                    leftColumn.show();
+                    rightColumn.addClass('blog__content_right_active-menu');
+                }
+            });
+        }
+    };
+})();
+
+if ($('.blog')) {
+    menu.setActive();
+    menu.fixed();
+    menu.flag();
+};
+
+/*------ END Blog-active --------------------------*/
+
+
+
 
 
 
